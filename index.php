4607868839e53cded32d9cc8f56da76b4c435c84
khaledrazouk <94979673+khaledrazouk@users.noreply.github.com>
2022-05-25 14:21:17 +0200

    test......

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<!DOCTYPE html> <html> <body>  <h1>Welcom to Simplon Cloud P#20</h1>  <?php echo "Hello World from test!!"; ?>  </body> </html>
+<!DOCTYPE html> <html> <body>  <h1>Welcom to Simplon Cloud P#20</h1>  <?php echo "Hello World from tetttst!!"; ?>  </body> </html>
